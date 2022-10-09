3b4ed348cbf22db1f77c9228df873bb623eed2f1
林寻 <3056726345@qq.com>
2022-10-09 16:28:38 +0800

    框架基本模型

---
+++ src/main/java/com/xingchen/ujcmsitems/cms/UjcmsItemsApplication.java
@@ -0,0 +1,16 @@
+package com.xingchen.ujcmsitems.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xing'chen
+ */
+@SpringBootApplication
+public class UjcmsItemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UjcmsItemsApplication.class, args);
+    }
+
+}
