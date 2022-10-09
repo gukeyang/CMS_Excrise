fb36c6487ef934c9c97461d6ba33ab91c19561b2
杨赛雅 <10715064+yang-saiya@user.noreply.gitee.com>
2022-10-09 16:55:45 +0800

    杨赛雅

--- src/main/java/com/ujcmsitems/UjcmsItemsApplication.java
+++ src/main/java/com/ujcmsitems/UjcmsItemsApplication.java
@@ -1,4 +1,4 @@
-package com.xingchen.ujcmsitems.cms;
+package com.ujcmsitems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
