488086d1e66c179c93c0a33e78a77870f4e14841
yang-sangwu <2728771838@qq.com>
2022-11-12 21:21:07 +0800

    杨赛雅第88次push

--- src/main/java/com/ujcmsitems/core/controller/PictureController.java
+++ src/main/java/com/ujcmsitems/core/controller/PictureController.java
@@ -7,7 +7,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
@@ -18,7 +21,7 @@ import java.io.IOException;
  */
 @Api(tags = "picture")
 @RestController
-@RequestMapping("/picture")
+//@RequestMapping("/picture")
 public class PictureController {
     @Resource
     private PictureService pictureService;
@@ -26,11 +29,10 @@ public class PictureController {
     @ApiOperation(value = "上传图片")
     @PostMapping(value = "/addImg")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "file", value = "选择图片", required = true),
             @ApiImplicitParam(name = "imgName", value = "备注", required = true)
     })
     @ResponseBody
-    public R upload(@RequestParam("files") MultipartFile files, String imgName) throws IOException {
-        return pictureService.upload(files, imgName);
+    public R upload(@RequestParam("files") MultipartFile file, String imgName) throws IOException {
+        return pictureService.upload(file, imgName);
     }
 }
