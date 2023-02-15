6e66beba44a80c2fec4de7dac0f31c8c1bed5baa
gukeyang <1656473414@qq.com>
2023-02-15 21:51:15 +0800

    123

--- src/main/java/com/ujcmsitems/core/controller/RequestUser.java
+++ src/main/java/com/ujcmsitems/core/controller/RequestUser.java
@@ -3,7 +3,6 @@ package com.ujcmsitems.core.controller;
 
 import com.ujcmsitems.core.domain.Manager;
 import com.ujcmsitems.core.service.ManagerService;
-import com.ujcmsitems.utils.RespBean;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 @Controller
 @CrossOrigin
@@ -48,23 +46,4 @@ public class RequestUser {
         return "http://" + localAddr + ":" + serverPort;
     }
 
-
-    @ApiOperation(value = "获取现在时间")
-    @RequestMapping(value = "/date", method = RequestMethod.GET)
-    @ResponseBody
-    public String currentDate() {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss");
-        Date date = new Date(System.currentTimeMillis());
-        String format = formatter.format(date);
-        System.out.println(formatter.format(date));
-        return format;
-    }
-
-
-    @ApiOperation(value = "退出登录")
-    @PostMapping("/logout")
-    @ResponseBody
-    public RespBean logout() {
-        return RespBean.success("注销成功！");
-    }
 }
